8b1b6fd10f1f4351384943f8ffe47e4ad03ed424
Lukasz Szkup <lszkup@atlassian.com>
2024-12-19 17:13:42 +1000

    feat: day 19

--- years/2024/19/index.ts
+++ years/2024/19/index.ts
@@ -13,17 +13,104 @@ const DAY = 19;
 // data path    : /Users/lszkup/Development/advent-of-code/years/2024/19/data.txt
 // problem url  : https://adventofcode.com/2024/day/19
 
+function canFormDesign(design: string, patterns: string[]): boolean {
+	const patternSet = new Set(patterns);
+	const dp = new Array<boolean>(design.length + 1).fill(false);
+	const dp2 = new Array<number>(design.length + 1).fill(0);
+	dp[0] = true; // base case: empty string can always be formed
+
+	for (let i = 0; i < design.length; i++) {
+		if (!dp[i]) continue;
+
+		// Try all patterns
+		for (const pattern of patternSet) {
+			if (design.startsWith(pattern, i)) {
+				dp[i + pattern.length] = true;
+				dp2[i + pattern.length]++;
+			}
+		}
+	}
+
+	return dp[design.length];
+}
+
+function canFormDesign2(design: string, patterns: string[]): number {
+	const patternSet = new Set(patterns);
+	const dp = new Array<boolean>(design.length + 1).fill(false);
+	const dp2 = new Array<number>(design.length + 1).fill(0);
+	dp[0] = true; // base case: empty string can always be formed
+	dp2[0] = 1;
+
+	for (let i = 0; i < design.length; i++) {
+		if (!dp[i]) continue;
+
+		// Try all patterns
+		for (const pattern of patternSet) {
+			if (design.startsWith(pattern, i)) {
+				dp2[i + pattern.length] += dp2[i];
+				dp[i + pattern.length] = true;
+			}
+		}
+	}
+
+	return dp2[design.length];
+}
+
 async function p2024day19_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n").filter(x => x.length > 0);
+	const patterns = lines[0].split(", ");
+	const designs = lines.slice(1);
+
+	let possibleCount = 0;
+	for (const design of designs) {
+		if (canFormDesign(design, patterns)) {
+			possibleCount++;
+		}
+	}
+
+	return possibleCount;
 }
 
 async function p2024day19_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n").filter(x => x.length > 0);
+	const patterns = lines[0].split(", ");
+	const designs = lines.slice(1);
+
+	let possibleCount = 0;
+	for (const design of designs) {
+		possibleCount += canFormDesign2(design, patterns);
+	}
+
+	return possibleCount;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`,
+		expected: `6`,
+	}];
+	const part2tests: TestCase[] = [{
+		input: `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`,
+		expected: `16`,
+	}];
 
 	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
